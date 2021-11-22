a113988485ef2c763fa13c965c0dfe4de2c00f74
Edgar Ivan Gomez <edgar.i.gomez@gmail.com>
2021-11-22 12:50:11 -0600

    adds codeup web exercises

---
+++ strings.js
@@ -0,0 +1,13 @@
+var name = "Edgar Gomez";
+
+var favoritefood = "my favorite food is ribeye steaks\nand cheese.";
+
+console.log("Hi my name is " + name + " " + "and " + favoritefood);
+
+var hobby = "playing video games\nand reading fantasy novels.";
+
+console.log("My hobbies are " + hobby)
+
+var associates = "associates in science with a focus in biology\nalso on in arts with a focus on liberal arts."
+
+console.log("I also have an " + associates)
