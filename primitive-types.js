db550127673c2226372c2b5785cf39c7f489085b
Edgar Ivan Gomez <edgar.i.gomez@gmail.com>
2021-11-22 15:25:58 -0600

    adds working with data types file also update primitive types file

--- primitive-types.js
+++ primitive-types.js
@@ -11,3 +11,6 @@ console.log(typeof 0)
 console.log(typeof true)
 
 console.log(typeof 'true')
+
+console.log("1 is string")
+
---
+++ working-with-data-types.js
@@ -0,0 +1,90 @@
+//1
+console.log("Part 1")
+var a = 1;
+var b = a++;
+
+console.log(b);
+var c = ++a;
+
+console.log(c);
+// a = 1 b = 1, c = 3
+
+var d = "hello";
+var e = false;
+
+console.log(d++);
+console.log(e++);
+//d is NaN e is false
+
+var perplexed;
+console.log(perplexed + 2);
+// NaN
+
+var price = 2.7;
+console.log(price.toFixed(2));
+//2.70
+
+//var price2 = "2.7";
+//console.log(price2.toFixed(2));
+//NaN
+
+console.log(isNaN(0));
+//False
+console.log(isNaN(1));
+//False
+console.log(isNaN(""));
+//False
+console.log(isNaN("string"));
+//True
+console.log(isNaN("0"));
+//False
+console.log(isNaN("1"));
+//False
+console.log(isNaN("3.145"));
+//False
+console.log(isNaN(Number.MAX_VALUE));
+//False
+console.log(isNaN(Infinity));
+//False
+console.log(isNaN("true"));
+//True
+console.log(isNaN(true));
+//False
+console.log(isNaN("false"));
+//True
+console.log(isNaN(false));
+//Flase
+console.log(NaN == NaN);
+//False
+
+//2
+console.log("Part 2")
+
+//3
+console.log("Part 3")
+var price = 3;
+var littlemermaid = 3;
+var brotherbear = 5;
+var hercules = 1;
+var totalprice = price * (littlemermaid + brotherbear + hercules);
+
+console.log(totalprice);
+
+var google = 400;
+var amazon = 380;
+var facebook = 350;
+var paycheck = (6 * google) + (4 * amazon) + (10 * facebook);
+
+console.log(paycheck);
+
+studentcanjoinclass = classnotfull && fitsherschedule
+
+productofferapplied = (buy2items && notexpired) || (premiummember && notexpired)
+
+//4
+var username = 'codeup';
+var password = 'notastrongpassword';
+var passwordlong = password.length >= 5;
+var usernameinpassword = password.replace(username, 0)
+var usernamelength = username.length <= 20
+
