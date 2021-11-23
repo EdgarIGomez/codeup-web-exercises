5ab6c8a1f92b113c71bf0e94181ee82a6feb1644
Edgar Ivan Gomez <edgar.i.gomez@gmail.com>
2021-11-22 23:27:54 -0600

    adds 2 function files and a modified working with data type file

--- working-with-data-types.js
+++ working-with-data-types.js
@@ -24,38 +24,52 @@ var price = 2.7;
 console.log(price.toFixed(2));
 //2.70
 
-//var price2 = "2.7";
-//console.log(price2.toFixed(2));
-//NaN
+//var prices = "2.7";
+//console.log(prices.toFixed(2));
+//Doesn't work as prices is string not a number
 
 console.log(isNaN(0));
 //False
+
 console.log(isNaN(1));
 //False
+
 console.log(isNaN(""));
 //False
+
 console.log(isNaN("string"));
 //True
+
 console.log(isNaN("0"));
 //False
+
 console.log(isNaN("1"));
 //False
+
 console.log(isNaN("3.145"));
 //False
+
 console.log(isNaN(Number.MAX_VALUE));
 //False
+
 console.log(isNaN(Infinity));
 //False
+
 console.log(isNaN("true"));
 //True
+
 console.log(isNaN(true));
 //False
+
 console.log(isNaN("false"));
 //True
+
 console.log(isNaN(false));
 //Flase
+
 console.log(NaN == NaN);
 //False
+
 
 //2
 console.log("Part 2")
@@ -73,23 +87,31 @@ console.log(samples);
 var samples = samples.replace("Students", "Class");
 console.log(samples);
 
-console.log(samples.indexOf(c));
-// console.log(samples.indexOf(C)); doesnt work with capital letter
+console.log(samples.indexOf("c"));
+console.log(samples.indexOf("C"));
 
-console.log(samples.substring(6,12));
+console.log(samples.substring(samples.indexOf("C"),samples.indexOf("p") +1));
 //3
 console.log("Part 3")
+
 var price = 3;
+
 var littlemermaid = 3;
+
 var brotherbear = 5;
+
 var hercules = 1;
+
 var totalprice = price * (littlemermaid + brotherbear + hercules);
 
 console.log(totalprice);
 
 var google = 400;
+
 var amazon = 380;
+
 var facebook = 350;
+
 var paycheck = (6 * google) + (4 * amazon) + (10 * facebook);
 
 console.log(paycheck);
@@ -99,9 +121,23 @@ studentcanjoinclass = classnotfull && fitsherschedule
 productofferapplied = (buy2items && notexpired) || (premiummember && notexpired)
 
 //4
+console.log("Part 4")
+
 var username = 'codeup';
+
 var password = 'notastrongpassword';
+
+
 var passwordlong = password.length >= 5;
-var usernameinpassword = password.replace(username, 0)
+
+console.log(passwordlong)
+
+var usernameinpassword = password !== username
+
+console.log(usernameinpassword)
+
 var usernamelength = username.length <= 20
 
+console.log(usernamelength)
+
+var whitespace = (username.indexOf(0) !== " ") && (password.indexOf(0 !== " ")
