fb127375766a7fa323b5a5b95b9057ba91786e8b
Edgar Ivan Gomez <edgar.i.gomez@gmail.com>
2021-12-02 10:02:33 -0600

    moves file to right folder and adds modifications

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,21 +1,25 @@
- let oddRandomNumber;
-     do {
-         prompt("Choose an odd number between 1 - 50.");
-    }while (oddRandomNumber % 2 === 0)
 
-console.log(oddRandomNumber);
+ //     do {
+ //         prompt("Choose an odd number between 1 - 50.");
+ //    }while (oddRandomNumber % 2 === 0)
+
+
 // How to keep the inserted prompted number to be stored or to stop the prompt to be keep being looped while inside the
  // loop
-
+ let oddRandomNumber = prompt("Choose an odd number between 1 - 50");
+console.log("Number to skip is " + oddRandomNumber);
 for (var i = 0; i <= 50; i++){
-        if (i % 2 == 0){
+       if ((oddRandomNumber % 2 === 0) && oddRandomNumber <= 50){
+           let oddRandomNumber = prompt("Choose an odd number between 1 - 50"); // won't stop looping if wrong input is put in
+           continue;
+       } if (i % 2 == 0){
         continue;
-    } if (randomNumber == i){
-        console.log("Yikes! Skipping number: " + randomNumber)
+    } if (oddRandomNumber == i){
+        console.log("Yikes! Skipping number: " + oddRandomNumber);  //Now it uses the oddRandomNumber variable
     } else {
-        console.log("Here is an odd number: " + i)
+        console.log("Here is an odd number: " + i);
     } if (i > 50){
-        break
+        break;
     }
 }
 
--- js/while.js
+++ js/while.js
@@ -1,9 +1,9 @@
-var i = 1
-
-while (i < 65536){
-    console.log(i * 2);
-    i = (i * 2)
-}
+// var i = 1
+//
+// while (i < 65536){
+//     console.log(i * 2);
+//     i = (i * 2)
+// }
 
 var allCones = Math.floor(Math.random() * 50) + 50;
 console.log(allCones)
