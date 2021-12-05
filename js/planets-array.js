5dd48d6317f87e5063ef2dd524a417bd42ef5e9b
Edgar Ivan Gomez <edgar.i.gomez@gmail.com>
2021-12-05 06:55:17 -0600

    makes changes to one file and adds the planets strings js and html files

--- js/planets-array.js
+++ js/planets-array.js
@@ -10,30 +10,30 @@
      */
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
-    planets.unshift("The Sun")
+    planets.unshift("The Sun");
     console.log(planets);
 
     console.log('Adding "Pluto" to the end of the planets array.');
-    planets.push("Pluto")
+    planets.push("Pluto");
     console.log(planets);
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
-    planets.shift()
+    planets.shift();
     console.log(planets);
 
     console.log('Removing "Pluto" from the end of the planets array.');
-    planets.pop()
+    planets.pop();
     console.log(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
-    let indexOfEarth = planets.indexOf("Earth")
-    console.log(indexOfEarth)
+    let indexOfEarth = planets.indexOf("Earth");
+    console.log(indexOfEarth);
 
     console.log("Reversing the order of the planets array.");
-    planets.reverse()
+    planets.reverse();
     console.log(planets);
 
     console.log("Sorting the planets array.");
-    planets.sort()
+    planets.sort();
     console.log(planets);
 })();
