89a0ad8b0833c3fddb42b37ed0f4eafbacd7e405
Edgar Ivan Gomez <edgar.i.gomez03@gmail.com>
2022-01-20 13:47:06 -0600

    adds the modifications for jquery mouse events

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -2,6 +2,17 @@
 //     alert( 'The DOM has finished loading!' );
 // });
 
+let heading = $("#heading")
+let pOne = $("#p-one")
+let divOne = $("#div-one")
+let listItemOne = $("#item-one")
+let ps = $("p")
+let listItems = $("li")
+
+
+
+
+//Part 1
 // $("#heading").ready(function(){
 //     alert($("#heading").text())
 // })
@@ -13,12 +24,12 @@
 // $("#item-one").ready(function(){
 //     alert($("#div-one").text())
 // });
-
+//Part 2
 // $(".codeUp").css("border", "1px solid red");
 
 // $("#p-one").removeClass("codeUp")
-
-$("li").css("font-size", "20px")
+//Part 3
+// $("li").css("font-size", "20px")
 
 // $("h1").css("background-color", "yellow");
 //
@@ -26,8 +37,26 @@ $("li").css("font-size", "20px")
 //
 // $("li").css("background-color", "yellow");
 
-$("h1").ready(function(){
-    alert($("h1").text());
+// $("h1").ready(function(){
+//     alert($("h1").text());
+// })
+// //Part 4
+// $("h1, p, li").css("background-color", "yellow");
+
+heading.click(function(){
+    $(this).css("background-color", "red")
 })
 
-$("h1, p, li").css("background-color", "yellow");
+ps.dblclick(function(){
+    $(this).css("font-size", "18px")
+})
+
+listItems.hover(
+    function(){
+      $(this).css("color", "red")
+    },
+    function(){
+        $(this).css("color", "black")
+    }
+)
+
