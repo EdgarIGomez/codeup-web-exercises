ecd890c65b0308b67475c5e9522c736ceda0fbd9
Edgar Ivan Gomez <edgar.i.gomez03@gmail.com>
2022-01-20 10:46:47 -0600

    modifies jquery exercise files

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,3 +1,19 @@
-$(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
-});
+// $(document).ready(function() {
+//     alert( 'The DOM has finished loading!' );
+// });
+
+// $("#heading").ready(function(){
+//     alert($("#heading").text())
+// })
+
+// $("#div-one").ready(function(){
+//     alert($("#div-one").text())
+// });
+
+// $("#item-one").ready(function(){
+//     alert($("#div-one").text())
+// });
+
+$(".codeUp").css("border", "1px solid red");
+
+// $("#p-one").removeClass("codeUp")
